550959e6e4bf2d49c2b24a732e5b3010c9d69de4
KoFeeBriAN <coffeetk2017@gmail.com>
2021-06-24 16:44:53 +0700

    mod: add try-catch block

--- express-computing/app/index.js
+++ express-computing/app/index.js
@@ -22,38 +22,36 @@ app.post(`/`, async (req, res) => {
 		return res.status(400).json({ text, word, result: null, message: "Bad request" });
 
 	let result;
-	const response = await axios.post(
-		`http://redis-client:4000/get`,
-		{
+	let response;
+	try {
+		response = await axios.post(`http://redis-client:4000/get`, {
 			key: text,
-		},
-		{
-			headers: {
-				"Content-Type": "application/json",
-			},
-		}
-	);
-
-	if (response?.data?.message !== "OK") {
-		res.status(200).json({
-			message: "From cache",
-			text,
-			word,
-			result: response.data,
 		});
+
+		console.log("logs get data", response.data);
+
+		if (response?.data?.message !== "OK") {
+			return res.status(200).json({
+				message: "From cache",
+				text,
+				word,
+				result: response.data,
+			});
+		}
+	} catch (error) {
+		console.log("No cache");
+		console.error(error);
 	}
 
 	result = text.match(new RegExp(word, "g")).length;
 
-	await axios.post(
-		`http://redis-client:4000/set`,
-		{ key: text, value: result },
-		{
-			headers: {
-				"Content-Type": "application/json",
-			},
-		}
-	);
+	try {
+		response = await axios.post(`http://redis-client:4000/set`, { key: text, value: result });
+		console.log("logs set data", response.data);
+	} catch (error) {
+		console.log("Save to cache failed");
+		console.error(error);
+	}
 
 	res.status(200).json({
 		message: "OK",
