f0f2eeb60d952b68e88de129e8bb99bbfd4b0aba
KoFeeBriAN <coffeetk2017@gmail.com>
2021-06-24 18:32:33 +0700

    feat: work on docker-compose
    app work properly on docker compose

--- express-computing/app/index.js
+++ express-computing/app/index.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get(`/`, (req, res) => {
@@ -18,39 +20,48 @@ app.get(`/`, (req, res) => {
  */
 app.post(`/`, async (req, res) => {
 	const { text, word } = req.body;
+	// If no text or word provide, return Bad Request
 	if (!text || !word)
 		return res.status(400).json({ text, word, result: null, message: "Bad request" });
 
 	let result;
 	let response;
 	try {
+		// Check in Cache
 		response = await axios.post(`http://redis-client:4000/get`, {
-			key: text,
+			key: text + " " + word,
 		});
 
 		console.log("logs get data", response.data);
 
-		if (response?.data?.message !== "OK") {
+		// If found, reture result
+		if (response?.status === 200) {
 			return res.status(200).json({
-				message: "From cache",
+				message: "From Cache",
 				text,
 				word,
-				result: response.data,
+				result: response.data.data,
 			});
 		}
 	} catch (error) {
 		console.log("No cache");
-		console.error(error);
+		console.error(error.response);
 	}
 
-	result = text.match(new RegExp(word, "g")).length;
+	// Perform counting
+	result = text.match(new RegExp(word, "g"))?.length;
+	if (!result) result = 0;
 
 	try {
-		response = await axios.post(`http://redis-client:4000/set`, { key: text, value: result });
+		// Save to Cache
+		response = await axios.post(`http://redis-client:4000/set`, {
+			key: text + " " + word,
+			value: result,
+		});
 		console.log("logs set data", response.data);
 	} catch (error) {
 		console.log("Save to cache failed");
-		console.error(error);
+		console.error(error.response);
 	}
 
 	res.status(200).json({
